e08c9072d67eda0b93805f7f58224446bde5884e
Alex Raichev <alex@raichev.net>
2014-08-17 15:33:19 +1200

    Updated develop requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@ from distutils.core import setup
 
 setup(
     name='gtfs-toolkit',
-<<<<<<< HEAD
-    version='0.1.12',
-=======
-    version='0.2.1',
->>>>>>> 57d7fbb8d668f0644d7e5e77120b07f57375968f
+    version='0.2.2',
     author='Alexander Raichev',
     author_email='alex@raichev.net',
     packages=['gtfs_toolkit', 'gtfs_toolkit.tests'],
