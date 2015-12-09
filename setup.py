30c2d9a9c2a47011fc62bba05eb9233514318ef9
Alex Raichev <alex@raichev.net>
2015-12-09 15:39:57 +1300

    Updated dist

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='gtfstk',
-    version='2.1',
+    version='3',
     author='Alexander Raichev',
     author_email='alex@raichev.net',
     packages=['gtfstk', 'tests'],
