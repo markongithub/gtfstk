3c19e3fb8173894b01717c0fc26130ffa4847c08
Alex Raichev <alex@raichev.net>
2015-12-18 12:10:08 +1300

    Made development version of setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='gtfstk',
-    version='3.0.1',
+    version='develop',
     author='Alexander Raichev',
     author_email='alex@raichev.net',
     packages=['gtfstk', 'tests'],
