fa91c2d69009ddaac9c21c8484d42f3d9ab9adec
Alex Raichev <alex@raichev.net>
2016-11-24 14:43:22 +1300

    Updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='gtfstk',
-    version='6.0.0',
+    version='6.1.0',
     author='Alex Raichev',
     packages=['gtfstk', 'tests'],
     url='https://github.com/araichev/gtfstk',
@@ -10,8 +10,8 @@ setup(
     description='A Python 3.5 tool kit for processing General Transit Feed Specification (GTFS) data',
     long_description=open('README.rst').read(),
     install_requires=[
-        'Shapely>=1.5.1, <=1.5.15',
-        'pandas>=0.18.1, <=0.19.0',
+        'Shapely>=1.5.1,<1.6',
+        'pandas>=0.18.1,<0.20',
         'utm>=0.3.1',    
     ],
 )
