6626e513613094cf65647e84a1b645c0ba7aee25
Alex Raichev <araichev@users.noreply.github.com>
2018-03-21 16:39:25 +1300

    Updated setup README

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author='Alex Raichev',
     url='https://github.com/araichev/gtfstk',
     data_files = [('', ['LICENSE.txt'])],
-    description='A Python 3.4+ tool kit that analyzes General Transit'
+    description='A Python 3.5+ tool kit that analyzes General Transit'
       'Feed Specification (GTFS) data',
     long_description=readme,
     license=license,
